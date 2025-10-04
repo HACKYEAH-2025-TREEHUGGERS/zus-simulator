8bc35e43f9162eab43fa6374ab1c3ec7ee87d856
Jakub Kowalczyk <jakub.kowalczyk@mindpal.co>
2025-10-04 21:10:22 +0200

    backend: fun fact endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import retirementRoutes from './retirement.routes';
+import funFactsRoutes from './fun-facts.routes';
 
 const router = Router();
 
 // Mount route modules
 router.use('/retirement', retirementRoutes);
+router.use('/fun-facts', funFactsRoutes);
 
 export default router;
