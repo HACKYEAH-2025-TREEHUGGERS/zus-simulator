af1ce153bf3bbf75a2d943791882b44be6e90f8b
Marcin Niemczyk <m_niemczyk@outlook.com>
2025-10-05 01:49:11 +0200

    The most important calculation that will be fix in the next commit (waiting for csv)

--- backend/src/controllers/retirement.controller.ts
+++ backend/src/controllers/retirement.controller.ts
@@ -7,7 +7,7 @@ class RetirementController {
     const input = calculateRetirementSchema.parse(req.body);
 
     const result = {
-      expectedRetirementAge: null, //retirementService.getExpectedRetirementDate(input.gender, input.date),
+      expectedRetirementAge: await retirementService.calculateRetirementValue(input),
     };
 
     res.json(result);
